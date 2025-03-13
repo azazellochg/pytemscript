647939b86886eb4e951cbb0f0f405882dca10d30
Grigory Sharov <sharov.grigory@gmail.com>
2025-03-13 14:58:27 +0000

    minor changes

--- pytemscript/utils/constants.py
+++ pytemscript/utils/constants.py
@@ -3,14 +3,10 @@ SCRIPTING_STD = "TEMScripting.Instrument.1"
 SCRIPTING_ADV = "TEMAdvancedScripting.AdvancedInstrument.2"  # .1 for Titan TEM server < 2.15 ?
 SCRIPTING_LOWDOSE = "LDServer.LdSrv"
 SCRIPTING_TIA = "ESVision.Application"
-SCRIPTING_SEM_CCD = "SerialEMCCD.DMCamera"
 SCRIPTING_TECNAI_CCD = "TecnaiCCD.GatanCamera"
 SCRIPTING_TECNAI_CCD2 = "TecnaiCCDPlugin.GatanCamera"
-SCRIPTING_FEI_GATAN_REMOTING = "ThermoScientificTEMPlugin.Acquisition"
 FEG_REGISTERS = "FegRegisters.FegReg"
-
-LICENSE_ADV = "TEMAdvancedScripting.AdvancedInstrumentInternal"
-LICENSE_ADV_CAM = "TEMAdvancedScripting.CameraSettingsInternal"
+CALGETTER = "CalGetter.Calibrations"
 
 HEADER_DATA = b'DT'
 HEADER_MSG = b'MS'
